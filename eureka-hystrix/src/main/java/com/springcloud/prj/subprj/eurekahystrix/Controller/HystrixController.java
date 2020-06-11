7bc8a18733e96ebd84545140d73e0d10c8ff6bcb
Administrator <com_yxf@163.com>
2020-06-12 00:08:42 +0800

    v3.00

---
+++ eureka-hystrix/src/main/java/com/springcloud/prj/subprj/eurekahystrix/Controller/HystrixController.java
@@ -0,0 +1,30 @@
+package com.springcloud.prj.subprj.eurekahystrix.Controller;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.ribbon.proxy.annotation.Hystrix;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("")
+public class HystrixController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @HystrixCommand(fallbackMethod = "getDefaultData")
+    @GetMapping("")
+    public String getData(){
+        String url="";
+
+        return restTemplate.getForObject(url,String.class);
+    }
+
+    public String getDefaultData(){
+        return "";
+    }
+}
